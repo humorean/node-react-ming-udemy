000fa71b1f562199eab7220405ea51e7a309a59b
Ming Huang <humorean@gmail.com>
2019-03-08 17:01:02 -0800

    added comments

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,8 @@
 // Production keys here 
 // COMMIT THIS TO PRODUCTION
 module.export = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    googleClientID: process.env.GOOGLE_CLIENT_ID, // from google
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, //from google
+    mongoURI: process.env.MONGO_URI,  //from mlab
+    cookieKey: process.env.COOKIE_KEY //you type/create a random string with number a letters
 }
-// module.exports = {
-//     googleClientID: "146245926382-da67b7otsosio7ellqk5n7fg4scpm7sv.apps.googleusercontent.com",
-//     googleClientSecret: "ymZrEllUK6jK-tHNzvljPh9V",
-//     mongoUri: "mongodb://admin:123456aabb@ds155614.mlab.com:55614/test-jedi-skills",
-//     cookieKey: "hiiamrandomhellaRandomzcjvaiwerodhfsjladfj"
-// }
